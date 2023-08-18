f56ddf6ecbff42ee7b27c0bf646aec006adf4c6f
Mohammad Al-Nur Sarwer Arif <alnurarif@yahoo.com>
2023-08-19 03:27:49 +0600

    it adds the separate type emit

--- src/components/MultipleItem.vue
+++ src/components/MultipleItem.vue
@@ -1,14 +1,17 @@
 
 <script setup>
 defineProps(['name','age','country'])
-defineEmits(['update:name','update:age','update:country'])
+const emit = defineEmits(['update:name','update:age','update:country'])
+const updateCountry = (value) => {
+    emit('update:country', value)
+}
 </script>
 
 <template>
     <div>
         <input type="text" :value="name" @input="$emit('update:name',$event.target.value)">
         <input type="text" :value="age" @input="$emit('update:age',$event.target.value)">
-        <input type="text" :value="country" @input="$emit('update:country',$event.target.value)">
+        <input type="text" :value="country" @input="updateCountry($event.target.value)">
     </div>
 </template>
 
