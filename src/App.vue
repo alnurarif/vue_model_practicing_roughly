e802d8b41cd67e9ec0eb7248fdf1972f56649519
Mohammad Al-Nur Sarwer Arif <alnurarif@yahoo.com>
2023-08-19 02:22:27 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { reactive } from 'vue';
+import Person from './components/Person.vue'
+const person = reactive({
+  name : 'arif',
+  age : 33,
+  country : 'Bangladesh'
+})
+</script>
+
+<template>
+  {{person.name}} {{ person.age }} {{ person.country }}
+  <Person :person="person"/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,10 @@
+<script setup>
+    defineProps(['person'])
+</script>
+
+<template>
+    <input type="text" v-model="person.name">
+    <input type="text" v-model="person.age">
+    <input type="text" v-model="person.country">
+</template>
+
