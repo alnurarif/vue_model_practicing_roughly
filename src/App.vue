14d6cdb2efa7486824beda774675edf51ca588f1
Mohammad Al-Nur Sarwer Arif <alnurarif@yahoo.com>
2023-08-19 03:18:56 +0600

    it passes the multiple v-model:name, v-model:age like reactive value, which also changes the value to child's parent

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref, reactive } from 'vue';
 import Person from './components/Person.vue'
 import SingleItem from './components/SingleItem.vue'
+import MultipleItem from './components/MultipleItem.vue';
 const person = reactive({
   name : 'arif',
   age : 33,
@@ -14,14 +15,17 @@ const namea = ref('')
 <template>
   <p>{{person.name}} {{ person.age }} {{ person.country }}</p>
   <p>ref name : {{ namea }}</p>
-  <Person :person="person"/>
+  <!-- <Person :person="person"/> -->
   <br/>
   <!-- niche amra :name evabe kore pathaini karon evabe kore pathale SingleItem theke etake defineProps(['name'])
   diye receive korar por input e v-model="name" dile error dekhato. child e change korle parent e change hoto na. mane parent e 
   write able na. tai v-model hisebe pass korte hobe component e. erpor etake normal ekta variable hisebe recv korbo defineProps(['modelValue'])
   evabe. erpor input e <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" /> evabe likhbo. 
   ekhane @input ta hocche event ei event e modelValue ta update hobe target.value diye mane input tar udpated value diye-->
-  <SingleItem v-model="namea"/>
+  <!-- <SingleItem v-model="namea"/> -->
+  
+  <MultipleItem v-model:name="person.name" v-model:age="person.age" v-model:country="person.country" />
+  
 
 </template>
 
---
+++ src/components/MultipleItem.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+defineProps(['name','age','country'])
+defineEmits(['update:name','update:age','update:country'])
+</script>
+
+<template>
+    <div>
+        <input type="text" :value="name" @input="$emit('update:name',$event.target.value)">
+        <input type="text" :value="age" @input="$emit('update:age',$event.target.value)">
+        <input type="text" :value="country" @input="$emit('update:country',$event.target.value)">
+    </div>
+</template>
+
